455580359e80ce09443eb89d4f7345cf070d1a54
Aashish Kumar Jha <20bcs3872@cuchd.in>
2023-07-16 14:48:53 +0530

    added s3 config and multer object

---
+++ src/config/file-upload-s3-config.js
@@ -0,0 +1,28 @@
+const multer = require("multer");
+const multerS3 = require("multer-s3");
+const aws = require("aws-sdk");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+aws.config.update({
+  region: process.env.AWS_REGION,
+  secretAccessKey: process.env.SECRET_ACCESS_KEY,
+  accessKeyId: process.env.ACCESS_KEY_ID,
+});
+
+const s3 = new aws.s3();
+
+const upload = multer({
+  s3: s3,
+  bucket: process.env.BUCKET,
+  acl: "public-read",
+  metadata: function (req, file, cb) {
+    cb(null, { fieldName: file.fieldname });
+  },
+  key: function (req, file, cb) {
+    cb(null, Date.now().toString());
+  },
+});
+
+module.exports = upload;
